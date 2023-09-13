1634a8c95f8bcb73a607f771ab052aaaa7fa3cee
kunal118 <100676364+kunal118@users.noreply.github.com>
2023-09-13 05:32:47 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const AuthURL =
     client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
     scope:
       "streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state",
-    redirect_uri: "/",
+    redirect_uri: "https://harmony-music-three.vercel.app/",
   });
 
 const Login = () => {
